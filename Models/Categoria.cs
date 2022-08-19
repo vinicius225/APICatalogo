7890ca181b25fe1b181d5269f0ebf6867c043eaa
Vinicius Guilherme <vinicius.guilherme225@gmail.com>
2022-08-19 00:04:17 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+             Produtos = new Collection<Produto>();
+        }
+
+        [Key]
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty ;
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public double Preco { get; set; }
+        public float Estoque { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int IdCategoria { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
